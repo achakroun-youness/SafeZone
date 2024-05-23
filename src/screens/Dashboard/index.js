1271dd86c21ade337c57f5d63383feaee8ad5988
chaymaeoukhouyy <chaymae0510oukhouyy@gmail.com>
2024-05-23 11:48:26 +0100

    upda

--- src/screens/Dashboard/index.js
+++ src/screens/Dashboard/index.js
@@ -1,31 +1,40 @@
 import * as React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text , Image } from "react-native";
 import { Border, FontSize, Color, FontFamily } from "../../../GlobalStyles";
+import { MaterialIcons } from '@expo/vector-icons';
 
 const Dashboard = () => {
   return (
     <View style={styles.dashboard}>
+     
         <Text style={styles.securcity}>SecurCity</Text>
-      <Text style={styles.dangerInYour}>Stats in your area</Text>
+        
+        
+      <Text style={styles.dangerInYour}>Stats </Text>
       <View style={styles.statsContainer}>
-        <View style={[styles.statBox, styles.dangerBox]}>
-          <Text style={styles.statNumber}>5</Text>
-          <Text style={styles.statLabel}>Danger Zones</Text>
+      <View style={[styles.statBox, styles.dangerBox]}>
+          <MaterialIcons name="warning" size={60} color="red" style={styles.icon} />
+          <Text style={styles.statLabel}>Danger Zones </Text><Text style={styles.statNumber}>5</Text>
+
         </View>
-        <View style={[styles.statBox, styles.safetyBox]}>
-          <Text style={styles.statNumber}>90%</Text>
-          <Text style={styles.statLabel}>Safety Rate</Text>
+                <View style={[styles.statBox, styles.safetyBox]}>
+                <MaterialIcons name="check-circle" size={60} color="green" style={styles.icon} />
+
+          <Text style={styles.statLabel}>Safety Rate</Text><Text style={styles.statNumber}>90%</Text>
         </View>
       </View>
-      <Text style={styles.dangerInYourCountry}>Stats in your country</Text>
       <View style={styles.statsContainer}>
         <View style={[styles.statBox, styles.submittedBox]}>
-          <Text style={styles.statNumber}>15</Text>
-          <Text style={styles.statLabel}>Submitted Zones</Text>
+        <MaterialIcons name="location-pin" size={60} color="orange" style={styles.icon} />
+
+          <Text style={styles.statLabel}>Submitted Zones</Text><Text style={styles.statNumber}>15</Text>
+
         </View>
         <View style={[styles.statBox, styles.reportedBox]}>
-          <Text style={styles.statNumber}>2</Text>
-          <Text style={styles.statLabel}>Reported Incidents</Text>
+        <MaterialIcons name="notifications" size={60} color="#CC7722" style={styles.icon} />
+
+         
+          <Text style={styles.statLabel}>Reported Incidents</Text><Text style={styles.statNumber}>2</Text>
         </View>
       </View>
     </View>
@@ -51,6 +60,13 @@ const styles = StyleSheet.create({
     color: Color.labelColorLightPrimary,
     fontFamily: FontFamily.balooThambiMedium,
     marginBottom: 20,
+    textAlign: 'center', 
+    
+  },
+  icon: {
+    marginRight: 10,
+    marginBottom: -15,
+    marginLeft: 15
   },
   statsContainer: {
     flexDirection: "row",
@@ -67,23 +83,47 @@ const styles = StyleSheet.create({
   },
   dangerBox: {
     backgroundColor: "#ffa8a7",
+    width: 140, // Set width and height to create a circle
+    height: 140,
+    borderRadius: 70, // Half of the width or height to make a circle
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 10,
   },
   safetyBox: {
     backgroundColor: "rgba(0, 0, 0, 0.25)",
+    width: 140, // Set width and height to create a circle
+    height: 140,
+    borderRadius: 70, // Half of the width or height to make a circle
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 10,
   },
   submittedBox: {
     backgroundColor: "rgba(255, 190, 157, 0.64)",
+    width: 140, // Set width and height to create a circle
+    height: 140,
+    borderRadius: 70, // Half of the width or height to make a circle
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 10,
   },
   reportedBox: {
     backgroundColor: "rgba(255, 199, 40, 0.22)",
+    width: 140, // Set width and height to create a circle
+    height: 140,
+    borderRadius: 70, // Half of the width or height to make a circle
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 10,
   },
   statNumber: {
-    fontSize: FontSize.size_33xl,
+    fontSize: FontSize.size_xl,
     color: Color.labelColorLightPrimary,
     fontFamily: FontFamily.balooThambiMedium,
   },
   statLabel: {
-    fontSize: FontSize.size_7xl,
+    fontSize: FontSize.size_xl,
     color: Color.labelColorLightPrimary,
     fontFamily: FontFamily.balooThambiMedium,
     textAlign: "center",
@@ -94,6 +134,14 @@ const styles = StyleSheet.create({
     color: Color.labelColorLightPrimary,
     fontFamily: FontFamily.balooThambiMedium,
     marginBottom: 20,
+    textAlign: 'center', 
+  },
+  mapContainer: {
+    height: 200, // Adjust the height as needed
+    marginVertical: 10,
+    borderRadius:20,
+    overflow: 'hidden'
+
   },
 });
 
--- src/screens/Register/index.js
+++ src/screens/Register/index.js
@@ -17,7 +17,7 @@ const Register = ({ navigation }) => {
   const handleSignup = async() => {
     if (isChecked) {
 
-      const response = await fetch("http://10.10.1.101:3000/api/users", {
+      const response = await fetch("http://192.168.8.156:3000/api/users", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
